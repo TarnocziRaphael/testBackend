c50f9ab2318e62153ea88b9ba59dd8b705e94bd9
Raphael Tarnoczi <raphi.tarnoczi@gmail.com>
2024-09-23 21:07:45 +0200

    two test requests

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const dataBase = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/randomUser', (req,res) => {
+  const query = "SELECT * FROM user ORDER BY RAND() LIMIT 1;";
+  dataBase.query(query, (error, result) => {
+    if(error) {
+      res.status(500).send(error)
+    }
+    else {
+      res.status(200).send(result)
+    }
+  });  
 });
 
-module.exports = app;
+app.get('/allUser', (req,res) => {
+  const query = "SELECT * FROM user;";
+  dataBase.query(query, (error, result) => {
+    if(error) {
+      res.status(500).send(error)
+    }
+    else {
+      res.status(200).send(result)
+    }
+  });  
+});
+
+app.use((req, res) => {
+  res.status(404).send('404-Fehler');
+});
+
+const port = process.env.APP_PORT;
+app.listen(port, () => {
+  console.log('Server läuft auf Port '+port);
+});
